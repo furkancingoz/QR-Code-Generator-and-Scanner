08b09ba1b960589eca8e383f2c9847031af96034
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-12-08 22:21:47 +0300

    Initial Commit

---
+++ QR Code Generator and Scanner/QR_Code_Generator_and_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_Code_Generator_and_ScannerApp.swift
+//  QR Code Generator and Scanner
+//
+//  Created by Furkan Cingöz on 8.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct QR_Code_Generator_and_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
