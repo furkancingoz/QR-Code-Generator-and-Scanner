f21b409545494c14c2ff1f1c43c93d7b45538b6c
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-12-09 14:17:45 +0300

    offf

--- QR Code Generator and Scanner/ContenView.swift
+++ QR Code Generator and Scanner/ContenView.swift
@@ -22,103 +22,95 @@ private let onboardingSteps = [
 struct ContenView: View {
   @State private var isActive: Bool = false
   @State private var showQRGenerate = false // QRGenerate ekranını göstermek için kullanılan bir state
-  
-  
+
+
   @State private var currentStep = 0
-  
-  
+
+
   init() {
     UIScrollView.appearance().bounces = false
   }
   @AppStorage("isWelcomeScreenOver") var isWelcomeScreenOver = false
   @Environment(\.dismiss) var dismiss
   @State var isPressed: Bool = false
-  @State var checkWelcomeScreen: Bool = false
+  
   var body: some View {
-    VStack {
-      if checkWelcomeScreen {
-        QRGenerateView()
-      } else {
-        ContenView()
-      }
-    }.onAppear {
-      checkWelcomeScreen = isWelcomeScreenOver
-      NavigationView {
-        VStack{
-          HStack{
-            Spacer()
-            Button(action: {
-              
-              self.showQRGenerate = true
-            }, label: {
-              Text("Skip")
-                .padding()
-                .foregroundColor(.gray)
-            })
-          }
-          
-          
-          
-          
-          TabView(selection: $currentStep){
-            ForEach(0..<onboardingSteps.count){ items in
-              VStack{
-                Image(systemName: onboardingSteps[items].image)
-                  .resizable()
-                  .frame(width: 350,height: 350)
-                  .foregroundStyle(.primary)
-                
-                
-                Text(onboardingSteps[items].title)
-                  .font(.title)
-                  .bold()
-                
-                Text(onboardingSteps[items].desc)
-                  .font(.subheadline)
-                  .multilineTextAlignment(.center)
-                  .padding(.horizontal,32)
-                
-              }
-              .tag(items)
-            }
-          }
-          .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-          
-          HStack {
-            ForEach(0..<onboardingSteps.count){ item in
-              if item == currentStep {
-                Rectangle()
-                  .frame(width: 20,height: 10)
-                  .cornerRadius(10)
-                  .foregroundColor(.blue)
-              } else {
-                Circle()
-                  .frame(width: 10,height: 10)
-                  .foregroundColor(.gray)
-              }
-            }
-          }
-          .padding(.bottom,25)
-          
-          Button(action:{
-            if self.currentStep < onboardingSteps.count - 1 {
-              self.currentStep += 1
-            } else {
-              self.isActive = true
-              isPressed = true
-              isWelcomeScreenOver = true// Ekleme: Get Started'a basıldığında QRGenerate ekranını göster
-            }
-          }) {
-            Text(currentStep < onboardingSteps.count - 1 ? "Next" : "Get Started")
-              .padding(15)
-              .frame(maxWidth: .infinity)
-              .background(Color.blue)
-              .cornerRadius(16)
-              .padding(.horizontal,16)
-              .foregroundColor(.white)
-          }.background(NavigationLink(destination: QRGenerateView().navigationBarHidden(true), isActive: $isActive) { EmptyView() }.hidden())
-          
+    
+    NavigationView {
+      VStack{
+        HStack{
+          Spacer()
+          Button(action: {
+
+            self.showQRGenerate = true
+          }, label: {
+            Text("Skip")
+              .padding()
+              .foregroundColor(.gray)
+          })
         }
+
+
+
+
+        TabView(selection: $currentStep){
+          ForEach(0..<onboardingSteps.count){ items in
+            VStack{
+              Image(systemName: onboardingSteps[items].image)
+                .resizable()
+                .frame(width: 350,height: 350)
+                .foregroundStyle(.primary)
+
+
+              Text(onboardingSteps[items].title)
+                .font(.title)
+                .bold()
+
+              Text(onboardingSteps[items].desc)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal,32)
+
+            }
+            .tag(items)
+          }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+        HStack {
+          ForEach(0..<onboardingSteps.count){ item in
+            if item == currentStep {
+              Rectangle()
+                .frame(width: 20,height: 10)
+                .cornerRadius(10)
+                .foregroundColor(.blue)
+            } else {
+              Circle()
+                .frame(width: 10,height: 10)
+                .foregroundColor(.gray)
+            }
+          }
+        }
+        .padding(.bottom,25)
+
+        Button(action:{
+          if self.currentStep < onboardingSteps.count - 1 {
+            self.currentStep += 1
+          } else {
+            self.isActive = true
+            isPressed = true
+            isWelcomeScreenOver = true// Ekleme: Get Started'a basıldığında QRGenerate ekranını göster
+          }
+        }) {
+          Text(currentStep < onboardingSteps.count - 1 ? "Next" : "Get Started")
+            .padding(15)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .cornerRadius(16)
+            .padding(.horizontal,16)
+            .foregroundColor(.white)
+        }.background(NavigationLink(destination: QRGenerateView().navigationBarHidden(true), isActive: $isActive) { EmptyView() }.hidden())
+
       }
     }
   }
