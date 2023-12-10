d5b67e6831337f2b6da85fb2811136b8febd38d1
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-12-11 00:09:35 +0300

    i think delegate ok

--- QR Code Generator and Scanner/QRScannerDelegate.swift
+++ QR Code Generator and Scanner/QRScannerDelegate.swift
@@ -6,13 +6,15 @@
 //
 
 import SwiftUI
+import AVKit
 
-struct QRScannerDelegate: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class QRScannerDelegate: NSObject,ObservableObject, AVCaptureMetadataOutputObjectsDelegate {
+  func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+    if let metaObject = metadataObjects.first {
+      guard let readableObject = metaObject as? AVMetadataMachineReadableCodeObject else { return }
+      guard let scannedCode =  readableObject.stringValue else { return }
+      print(scannedCode)
     }
+  }
 }
 
-#Preview {
-    QRScannerDelegate()
-}
