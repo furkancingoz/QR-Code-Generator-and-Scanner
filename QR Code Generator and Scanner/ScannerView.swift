a7262c43dcde3703299dfcce0f8daeba4a83f6ef
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-12-10 18:35:29 +0300

    create a scanner

--- QR Code Generator and Scanner/ScannerView.swift
+++ QR Code Generator and Scanner/ScannerView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ScannerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    VStack{
+      Button{
+
+      } label: {
+        Image(systemName: "xmark")
+          .font(.title3)
+          .foregroundColor(.blue)
+      }
+      .frame(maxWidth: .infinity,alignment: .leading)
+
+      Text("Place the QR Code inside the area")
+        .font(.title3)
+        .foregroundColor(.white.opacity(0.8))
+        .padding(.top,20)
+
+      Text("Scanning will start automatically")
+        .font(.callout)
+        .foregroundStyle(.gray)
+
+      Spacer(minLength: 0)
+
+      //MARK: - SCANNER
+
+      Spacer(minLength: 0)
+
+
+      Button{
+        //action
+      } label: {
+        Image(systemName: "qrcode.viewfinder")
+          .font(.largeTitle)
+          .foregroundColor(.gray)
+      }
+
+      Spacer(minLength: 45)
     }
+    .padding(15)
+  }
 }
 
 #Preview {
-    ScannerView()
+  ScannerView()
 }
